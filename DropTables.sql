ed4b3eff654132ea438cbb528ff03864e222f039
Crowy-o <63596025+Crowy-o@users.noreply.github.com>
2020-05-25 21:51:12 +0200

    Creating DropTable

---
+++ DropTables.sql
@@ -0,0 +1,10 @@
+DROP TABLE subject_student;
+DROP TABLE specialityPlan;
+DROP TABLE planSubject;
+DROP TABLE student;
+DROP TABLE st_group;
+DROP TABLE subject;
+DROP TABLE studyPlan;
+DROP TABLE speciality;
+DROP TABLE deanary;
+DROP TABLE department;
