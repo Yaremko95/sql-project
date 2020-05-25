4adda9b4a53162d139f24b39c5cc7ef5a0a448c2
Crowy-o <63596025+Crowy-o@users.noreply.github.com>
2020-05-25 20:38:27 +0200

    Create tables

--- CreateTable.sql
+++ CreateTable.sql
@@ -1,70 +1,68 @@
 
 CREATE TABLE department (
-code     VARCHAR (6)     CONSTRAINT pk_department         PRIMARY KEY,
-nameDp   VARCHAR (50)    CONSTRAINT nn_nameDp_depatment   NOT NULL
+code      VARCHAR (6)     CONSTRAINT pk_department         PRIMARY KEY,
+nameDp    VARCHAR (50)    CONSTRAINT nn_nameDp_depatment   NOT NULL
 );
 
 
 CREATE TABLE deanary (
-code     VARCHAR (6)     CONSTRAINT pk_deanary            PRIMARY KEY,
-codeDep  VARCHAR (6)     CONSTRAINT fk_department         REFERENCES department,
-nameDea  VARCHAR (50)    CONSTRAINT nn_nameDea_deanary    NOT NULL
+code      VARCHAR (6)     CONSTRAINT pk_deanary            PRIMARY KEY,
+codeDep   VARCHAR (6)     CONSTRAINT fk_department         REFERENCES department,
+nameDea   VARCHAR (50)    CONSTRAINT nn_nameDea_deanary    NOT NULL
 );
 
 CREATE TABLE speciality (
-code     VARCHAR (6)     CONSTRAINT pk_speciality         PRIMARY KEY,
-codeDea  VARCHAR (6)     CONSTRAINT fk_deanaery           REFERENCES deanary,
-nameSpe  VARCHAR (50)    CONSTRAINT nn_nameSpe_speciality NOT NULL
+code      VARCHAR (6)     CONSTRAINT pk_speciality         PRIMARY KEY,
+codeDea   VARCHAR (6)     CONSTRAINT fk_deanaery           REFERENCES deanary,
+nameSpe   VARCHAR (50)    CONSTRAINT nn_nameSpe_speciality NOT NULL
 );
 
 CREATE TABLE studyPlan (
-code     VARCHAR (6)     CONSTRAINT pk_study              PRIMARY KEY
+code      VARCHAR (6)     CONSTRAINT pk_study              PRIMARY KEY
 );
 
 CREATE TABLE specialityPlan (
-codeSpe  VARCHAR (6)     CONSTRAINT fk_speciality         REFERENCES speciality,
-codePlan VARCHAR (6)     CONSTRAINT fk_studyPlan          REFERENCES studyPlan
+codeSpe   VARCHAR (6)     CONSTRAINT fk_speciality         REFERENCES speciality,
+codePlan  VARCHAR (6)     CONSTRAINT fk_studyPlan          REFERENCES studyPlan
 );
 
  CREATE TABLE subject_student (
-            codeSub VARCHAR(6) ,
-            dniStud VARCHAR(6),
-            grade NUMBER(2,1) 
-        
-        );
-        
-        CREATE TABLE student (
-           dni VARCHAR(6) CONSTRAINT pk_dni PRIMARY KEY,
-           codeGroup VARCHAR(6),
-           nameSt VARCHAR(50) NOT NULL,
-           surname VARCHAR(50) NOT NULL,
-           address VARCHAR(100) NOT NULL,
-           email VARCHAR(30) NOT NULL,
-           phone VARCHAR(15) NOT NULL,
-           yearOfStudy NUMBER(1) NOT NULL );
+codeSub   VARCHAR(6) ,
+dniStud   VARCHAR(6),
+grade     NUMBER(2,1) 
+);
+
+CREATE TABLE student (
+dni       VARCHAR(6)       CONSTRAINT pk_dni               PRIMARY KEY,
+codeGroup VARCHAR(6),
+nameSt    VARCHAR(50) NOT NULL,
+surname   VARCHAR(50) NOT NULL,
+address   VARCHAR(100) NOT NULL,
+email     VARCHAR(30) NOT NULL,
+phone     VARCHAR(15) NOT NULL,
+yearOfStudy NUMBER(1) NOT NULL 
+);
            
-           
-        CREATE TABLE st_group (
-            code VARCHAR(6) CONSTRAINT pk_code PRIMARY KEY,
-            codePlan VARCHAR(6)  --CODE OF A STUDY PLAN
-        );
-        
-        -- 
-        CREATE TABLE planSubject (
-            codePlan    VARCHAR(6) ,
-            codeSub VARCHAR(6),
-            hoursLecture NUMBER(3),
-            hoursPractice NUMBER(3),
-            hoursLabClasses NUMBER(3),
-            hoursCourseWork NUMBER(3),
-            kindExam VARCHAR (1) CONSTRAINT ck_kindExam_studyPlan CHECK ( kindExam = 'P' OR kindExam = 'T')  /*P - practice, T - theory*/
-            
-        );
-        CREATE TABLE subject (
-            code VARCHAR(6) CONSTRAINT pk_sub_code  PRIMARY KEY,
-            title VARCHAR(50),
-            credits NUMBER(2) 
-            );
+CREATE TABLE st_group (
+code      VARCHAR(6)        CONSTRAINT pk_code             PRIMARY KEY,
+codePlan  VARCHAR(6)  --CODE OF A STUDY PLAN
+);
+
+CREATE TABLE planSubject (
+codePlan  VARCHAR(6) ,
+codeSub   VARCHAR(6),
+hoursLecture  NUMBER(3),
+hoursPractice NUMBER(3),
+hoursLabClasses NUMBER(3),
+hoursCourseWork NUMBER(3),
+kindExam  VARCHAR (1)       CONSTRAINT ck_kindExam_studyPlan CHECK ( kindExam = 'P' OR kindExam = 'T')  /*P - practice, T - theory*/
+);
+
+CREATE TABLE subject (
+code      VARCHAR(6)        CONSTRAINT pk_sub_code        PRIMARY KEY,
+title     VARCHAR(50),
+credits   NUMBER(2) 
+);
     
     --create triggers for primary keys
     CREATE SEQUENCE dni_auto_id start with 1;
---
+++ InsertData.sql
@@ -0,0 +1,24 @@
+/*Department*/
+INSERT INTO department VALUES ( ,'Computer Science and Technology');
+
+/*Deanary*/
+INSERT INTO deanary  VALUES ( , ,'Software engineering');
+INSERT INTO deanary  VALUES ( , ,'Hardware engineering');
+INSERT INTO deanary  VALUES ( , ,'Computer security');
+
+/*Speciality*/
+    /*Software deanary*/
+INSERT INTO speciality  VALUES ( , ,'Graphics and human-computer interaction');
+INSERT INTO speciality  VALUES ( , ,'Programming');
+    /*Hardsware deanary*/
+INSERT INTO speciality  VALUES ( , ,'Computer architecture');
+INSERT INTO speciality  VALUES ( , ,'Distributed systems');
+    /*Security deanary*/
+INSERT INTO speciality  VALUES ( , ,'Security');
+INSERT INTO speciality  VALUES ( , ,'Networking and wireless communication');
+
+/*Study Plan*/
+INSERT INTO studyPlan VALUES ( );
+
+/*SpecialityPlan*/
+INSERT INTO SpecialityPlan VALUES ( , ,);
