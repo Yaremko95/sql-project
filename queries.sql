019f6308db6ca8f112ec11d8c26e14da54b4e8b5
tetiana <tetianayaremko@gmail.com>
2020-05-26 10:39:47 +0200

    query

--- queries.sql
+++ queries.sql
@@ -4,7 +4,7 @@ INSERT INTO  subject_student (codeSub, dniStud, grade) SELECT ps.codeSub, s.dni,
  FROM planSubject ps, student s WHERE ps.codePlan=
         (SELECT codePlan FROM st_group g WHERE g.code = s.codeGroup);
 
-/*gets the highest average grade for practice exams*/
+/*gets student's highest average grade for practice exams*/
 SELECT MAX(avg)  FROM 
                     (SELECT dniStud, SUM(grade)/COUNT(dniStud) AS avg 
                     FROM subject_student 
@@ -13,3 +13,15 @@ SELECT MAX(avg)  FROM
                                     FROM planSubject 
                                     WHERE kindExam='P') 
                     GROUP BY dniStud);
+
+    /*gets list of students from each group who never took practice exam*/
+    SELECT  codeGroup, LISTAGG(nameSt || ' ' || COALESCE(surname, ' '), '; ') WITHIN GROUP (ORDER BY codeGroup) AS students  
+    FROM student WHERE dni IN (
+                                SELECT dniStud
+                                FROM subject_student 
+                                WHERE codeSub NOT IN(
+                                                    SELECT codeSub 
+                                                    FROM planSubject 
+                                                    WHERE kindExam='P')) 
+    GROUP BY codeGroup
+    ORDER BY codeGroup ;
