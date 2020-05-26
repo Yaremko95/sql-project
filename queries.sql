922c6d944fd9c89e85f6a9deeb9b1afca1c1df60
tetiana <tetianayaremko@gmail.com>
2020-05-26 10:02:50 +0200

    query

--- queries.sql
+++ queries.sql
@@ -3,3 +3,13 @@
 INSERT INTO  subject_student (codeSub, dniStud, grade) SELECT ps.codeSub, s.dni, DBMS_RANDOM.VALUE (0, 10)
  FROM planSubject ps, student s WHERE ps.codePlan=
         (SELECT codePlan FROM st_group g WHERE g.code = s.codeGroup);
+
+/*gets the highest average grade for practice exams*/
+SELECT MAX(avg)  FROM 
+                    (SELECT dniStud, SUM(grade)/COUNT(dniStud) AS avg 
+                    FROM subject_student 
+                    WHERE codeSub IN(
+                                    SELECT codeSub 
+                                    FROM planSubject 
+                                    WHERE kindExam='P') 
+                    GROUP BY dniStud);
